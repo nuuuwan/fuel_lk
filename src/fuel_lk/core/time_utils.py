6025a0f0413997707a5f2c502862816805f510b8
Nuwan I. Senaratna <nuuuwan@gmail.com>
2022-06-15 14:58:49 +0530

    Fixed timezone bug

--- src/fuel_lk/core/time_utils.py
+++ src/fuel_lk/core/time_utils.py
@@ -1,10 +1,13 @@
 from utils import timex
 
+FORMAT = '%Y-%m-%d %H:%M'
+TIMEZONE_OFFSET = timex.TIMEZONE_OFFSET_LK
 
 def get_times(t_str):
+
     try:
-        t_ut = timex.parse_time(t_str, '%Y-%m-%d %H:%M')
-        t_str2 = timex.format_time(t_ut)
+        t_ut = timex.parse_time(t_str, FORMAT, TIMEZONE_OFFSET)
+        t_str2 = timex.format_time(t_ut, FORMAT, TIMEZONE_OFFSET)
     except ValueError:
         t_ut = None
         t_str2 = None
