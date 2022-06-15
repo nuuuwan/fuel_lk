d5b2f71b1c0ab4ad6c8dce0bf354934e8a81d019
Nuwan I. Senaratna <nuuuwan@gmail.com>
2022-06-15 13:19:43 +0530

    Added DIR_DATA_HISTORY and DIR_DATA_LATEST

--- src/fuel_lk/common.py
+++ src/fuel_lk/common.py
@@ -1,5 +1,9 @@
+import os
+
 from utils import logx
 
 DIR_DATA = '/tmp/fuel_lk'
+DIR_DATA_HISTORY = os.path.join(DIR_DATA, 'history')
+DIR_DATA_LATEST = os.path.join(DIR_DATA, 'latest')
 
 log = logx.get_logger('fuel_lk')
