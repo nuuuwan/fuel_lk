93db7ed21e6c4b329632e7e69dadf169f6600d65
Nuwan I. Senaratna <nuuuwan@gmail.com>
2022-06-15 14:08:23 +0530

    Added DEFAULT_FUEL_TYPE and URL_API_BASE

--- src/fuel_lk/common.py
+++ src/fuel_lk/common.py
@@ -2,8 +2,12 @@ import os
 
 from utils import logx
 
+DEFAULT_FUEL_TYPE = 'p92'
+
 DIR_DATA = '/tmp/fuel_lk'
 DIR_DATA_HISTORY = os.path.join(DIR_DATA, 'history')
 DIR_DATA_LATEST = os.path.join(DIR_DATA, 'latest')
+
+URL_API_BASE = 'https://fuel.gov.lk/api/v1/sheddetails'
 
 log = logx.get_logger('fuel_lk')
