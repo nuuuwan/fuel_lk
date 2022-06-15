f91332855fd132fa2cc6cf2639ffd89fee4e588b
Nuwan I. Senaratna <nuuuwan@gmail.com>
2022-06-15 13:20:08 +0530

    Saved to latest and history

--- src/fuel_lk/workflows/upload_data.py
+++ src/fuel_lk/workflows/upload_data.py
@@ -1,16 +1,23 @@
 import os
 
-from fuel_lk.common import DIR_DATA
+from utils import timex
+
+from fuel_lk.common import DIR_DATA, DIR_DATA_HISTORY, DIR_DATA_LATEST, log
 from fuel_lk.scraper import scrape
 
 
 def before():
     os.system(f'rm -rf {DIR_DATA}')
     os.system(f'mkdir {DIR_DATA}')
+    os.system(f'mkdir {DIR_DATA_LATEST}')
+    os.system(f'mkdir {DIR_DATA_HISTORY}')
 
 
 def after():
-    os.system(f'rm -rf {DIR_DATA}')
+    time_id = timex.get_time_id()
+    dir_history_item = os.path.join(DIR_DATA_HISTORY, time_id)
+    os.system(f'cp -r {DIR_DATA}/latest {dir_history_item}')
+    log.info(f"Saved latest to {dir_history_item}")
 
 
 def pipeline():
