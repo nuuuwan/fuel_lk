ea129fd1ef7d35aef77f8e26d2e2e53cc580f807
Nuwan I. Senaratna <nuuuwan@gmail.com>
2022-06-16 08:52:44 +0530

    Added GIT_REPO_URL; Moved DIR_DATA_HISTORY and DIR_DATA_LATEST to workflow.common

--- src/fuel_lk/common.py
+++ src/fuel_lk/common.py
@@ -1,13 +1,13 @@
-import os
 
 from utils import logx
 
 DEFAULT_FUEL_TYPE = 'p92'
 
 DIR_DATA = '/tmp/fuel_lk'
-DIR_DATA_HISTORY = os.path.join(DIR_DATA, 'history')
-DIR_DATA_LATEST = os.path.join(DIR_DATA, 'latest')
+
 
 URL_API_BASE = 'https://fuel.gov.lk/api/v1/sheddetails'
+
+GIT_REPO_URL = 'https://github.com/nuuuwan/fuel_lk'
 
 log = logx.get_logger('fuel_lk')
