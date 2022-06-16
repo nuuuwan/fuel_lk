328308f3c5f7ab799601eddefbeec99e259f827d
Nuwan I. Senaratna <nuuuwan@gmail.com>
2022-06-16 09:44:47 +0530

    Added test_backpopulate

---
+++ src/fuel_lk/workflows/test_backpopulate.py
@@ -0,0 +1,10 @@
+
+
+from fuel_lk.workflows import common_workflows
+
+if __name__ == '__main__':
+    common_workflows.run_pipeline(
+        do_write_LEGACY_shed_status_list=True,
+        do_backpopulate=True,
+        do_test=True,
+    )
