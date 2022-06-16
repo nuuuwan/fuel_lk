2d7b5069e234190fbc6a1e6789bc16f85f0a1c97
Nuwan I. Senaratna <nuuuwan@gmail.com>
2022-06-16 09:19:05 +0530

    Fixed common_workflows BUG

--- src/fuel_lk/workflows/upload_data.py
+++ src/fuel_lk/workflows/upload_data.py
@@ -2,7 +2,7 @@
 
 from fuel_lk.base import Git
 from fuel_lk.common import DIR_DATA, GIT_REPO_URL
-from fuel_lk.workflows import common
+from fuel_lk.workflows import common_workflows
 
 
 def run_pipeline():
@@ -10,14 +10,17 @@ def run_pipeline():
     git.clone(DIR_DATA, force=True)
     git.checkout('data')
 
-    shed_list = common.scrape_sheds()
-    extended_shed_list = common.scrape_and_write_shed_statuses(shed_list)
+    shed_list = common_workflows.scrape_sheds()
+    shed_list = shed_list[:10]
+    extended_shed_list = common_workflows.scrape_and_write_shed_statuses(
+        shed_list)
 
     # TODO: Replace
-    common.write_LEGACY(extended_shed_list)
+    common_workflows.write_LEGACY_shed_status_list(extended_shed_list)
+    common_workflows.write_extened_status_list()
 
-    time_id = common.copy_latest_to_history()
-    common.write_readme(shed_list, time_id)
+    time_id = common_workflows.copy_latest_to_history()
+    common_workflows.write_readme(shed_list, time_id)
 
 
 if __name__ == '__main__':
